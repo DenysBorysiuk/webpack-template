76a2d463d2e4f8f52d3e8b45b48f352b80f2ac95
Denys Borysiuk <denisborisiuk@gmail.com>
2024-08-13 14:36:26 +0300

    add webpack cleanUp plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = env => {
   return {
     mode: env.mode ?? 'development',
+
     entry: path.resolve(__dirname, 'src', 'index.js'),
+
     output: {
       filename: 'bundle.[contenthash].js',
       path: path.resolve(__dirname, 'build'),
     },
+
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'src', 'template.html'),
         filename: 'index.html',
       }),
+      new CleanWebpackPlugin(),
     ],
+
     module: {
       rules: [
         {
